fd19c275aab6f9e2dbb1e103ca7d49f134d20974
Chye-Xian <chyexian.huang@gmail.com>
2016-05-12 02:36:29 -0700

    game state transfered to choice view

--- app/controllers/montyhall_controller.rb
+++ app/controllers/montyhall_controller.rb
@@ -1,15 +1,14 @@
 class MontyhallController < ApplicationController
   include MontyhallHelper
   def index
-    @@game = Game.new()
-    @@game.save()
-    @@game.doors.create(populate_door("door1"))
-    @@game.doors.create(populate_door("door2"))
-    @@game.doors.create(populate_door("door3"))
-    @@game.save()
+    @game = Game.new()
+    @game.save()
+    @game.doors.create(populate_door("door1"))
+    @game.doors.create(populate_door("door2"))
+    @game.doors.create(populate_door("door3"))
   end
 
   def choice
-    #@game = params[:first]
+    @game = Game.find(params[:game])
   end
 end
